6f0ebf0e59307cc0925d0cf47558b93313f9b736
ayu-sh-kr <akjaiswal2003@gmail.com>
2024-08-16 23:02:04 +0530

    Initial Commit -> Project init

---
+++ src/test/java/dev/archimedes/coursebackend/CourseBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.archimedes.coursebackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CourseBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
