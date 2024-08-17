b73ea2222e7cb19c24b2a65fb171d30694792616
ayu-sh-kr <akjaiswal2003@gmail.com>
2024-08-17 21:20:35 +0530

    Update DeliveryFetchedRecord.java: Fix epoch time zone conversion

--- src/main/java/dev/archimedes/coursebackend/delivery/dto/DeliveryFetchedRecord.java
+++ src/main/java/dev/archimedes/coursebackend/delivery/dto/DeliveryFetchedRecord.java
@@ -3,7 +3,7 @@ package dev.archimedes.coursebackend.delivery.dto;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
 import java.time.LocalDateTime;
-import java.time.ZoneOffset;
+import java.time.ZoneId;
 
 public record DeliveryFetchedRecord(
         Integer id,
@@ -12,11 +12,11 @@ public record DeliveryFetchedRecord(
 
         @JsonProperty("course_id")
         Integer courseId,
-        @JsonProperty("create_time")
+        @JsonProperty("created_on")
         long createTime
 ) {
 
     public DeliveryFetchedRecord(Integer id, String year, String semester, Integer courseId, LocalDateTime time) {
-        this(id, year, semester, courseId, time.toEpochSecond(ZoneOffset.of("IST")));
+        this(id, year, semester, courseId, time.atZone(ZoneId.of("Asia/Kolkata")).toEpochSecond());
     }
 }
