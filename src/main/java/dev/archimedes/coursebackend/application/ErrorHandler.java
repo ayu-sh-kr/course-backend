0669b4ec4f755b38987e9a8099792139f17ab355
ayu-sh-kr <akjaiswal2003@gmail.com>
2024-08-17 21:18:09 +0530

    Update ErrorHandler.java:  Added exception handling for HttpMediaTypeNotAcceptableException.class exception

--- src/main/java/dev/archimedes/coursebackend/application/ErrorHandler.java
+++ src/main/java/dev/archimedes/coursebackend/application/ErrorHandler.java
@@ -2,10 +2,15 @@ package dev.archimedes.coursebackend.application;
 
 import dev.archimedes.coursebackend.application.api.APIResponse;
 import dev.archimedes.coursebackend.application.exception.CourseException;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.HttpMediaTypeNotAcceptableException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+@Slf4j
 @RestControllerAdvice
 public class ErrorHandler {
 
@@ -16,5 +21,12 @@ public class ErrorHandler {
                 APIResponse.ofMessage(exception.getMessage()),
                 exception.getStatus()
         );
+    }
+
+    @ResponseBody
+    @ExceptionHandler(HttpMediaTypeNotAcceptableException.class)
+    public String handleHttpMediaTypeNotAcceptableException(HttpMediaTypeNotAcceptableException exception) {
+        System.out.println(exception.getMessage());
+        return "acceptable MIME type:" + MediaType.APPLICATION_JSON_VALUE;
     }
 }
