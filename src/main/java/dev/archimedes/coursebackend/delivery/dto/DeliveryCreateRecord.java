5dc4fe7053d363b2c074608f6558a7f89aae53fd
ayu-sh-kr <akjaiswal2003@gmail.com>
2024-09-01 18:14:08 +0530

    Update DeliveryCreateRecord.java
    Switch back the changes

--- src/main/java/dev/archimedes/coursebackend/delivery/dto/DeliveryCreateRecord.java
+++ src/main/java/dev/archimedes/coursebackend/delivery/dto/DeliveryCreateRecord.java
@@ -4,8 +4,7 @@ import jakarta.validation.constraints.Pattern;
 
 public record DeliveryCreateRecord(
 
-
-        @Pattern(regexp = "^(FIRST | SECOND | THIRD | FOURTH | FIFTH | SIXTH | SEVENTH | EIGHTH)")
+        @Pattern(regexp = "\\d{4}", message = "Year must be a four-digit number")
         String year,
 
         @Pattern(regexp = "^(I|II|III|IV|V|VI|VII|VIII)", message = "Semester must be a Roman numeral between I and VII")
